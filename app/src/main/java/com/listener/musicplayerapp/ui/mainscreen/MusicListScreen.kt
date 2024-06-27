992688524da6a17973bcea61cbb020a6f39dee3f
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-06-27 16:08:32 +0400

    Added layouts of song's card, list and search bar

---
+++ app/src/main/java/com/listener/musicplayerapp/ui/mainscreen/MusicListScreen.kt
@@ -0,0 +1,30 @@
+package com.listener.musicplayerapp.ui.mainscreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+private fun songs(): List<String> {
+    return List(1000) { "Song $it" }
+}
+
+@Composable
+fun MainListScreen(
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Spacer(modifier = Modifier.height(24.dp))
+        SearchBar()
+        SongList(songs = songs())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainListScreenPreview() {
+    MainListScreen()
+}
---
+++ app/src/main/java/com/listener/musicplayerapp/ui/mainscreen/SongCard.kt
@@ -0,0 +1,81 @@
+package com.listener.musicplayerapp.ui.mainscreen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SongContent(
+    songName: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.background,
+            ),
+            onClick = { /*TODO*/ }
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 10.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .clip(shape = RoundedCornerShape(2.dp))
+                        .background(color = MaterialTheme.colorScheme.primary)
+                        .size(50.dp),
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = null
+                )
+                Column {
+                    Text(
+                        text = songName,
+                        modifier = Modifier
+                            .width(200.dp),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(text = "Author")
+                }
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        IconButton(
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
+    }
+}
