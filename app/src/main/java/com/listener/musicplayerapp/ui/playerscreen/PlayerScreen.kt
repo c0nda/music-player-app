49a8bbcfdb6f00f8c9cd48645f2a83cec292d7b9
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-06-30 15:37:51 +0400

    Added ConstraintLayout for alignment of image and song name

--- app/src/main/java/com/listener/musicplayerapp/ui/playerscreen/PlayerScreen.kt
+++ app/src/main/java/com/listener/musicplayerapp/ui/playerscreen/PlayerScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun PlayerScreen(
@@ -40,27 +43,44 @@ fun PlayerScreen(
                 contentDescription = null
             )
         }
-        Image(
+        ConstraintLayout(
             modifier = Modifier
-                .size(280.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
-                .background(color = MaterialTheme.colorScheme.primary)
-                .align(Alignment.CenterHorizontally),
-            imageVector = Icons.Default.Warning,
-            contentDescription = null
-        )
-        Text(
-            modifier = Modifier
-                .padding(start = 20.dp),
-            text = "Song Name",
-            style = MaterialTheme.typography.titleLarge
-        )
-        Text(
-            modifier = Modifier
-                .padding(start = 20.dp),
-            text = "Author",
-            style = MaterialTheme.typography.bodyMedium
-        )
+                .fillMaxWidth()
+        ) {
+            val (image, textTitle, textAuthor) = createRefs()
+            Image(
+                modifier = Modifier
+                    .constrainAs(image) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .size(280.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = MaterialTheme.colorScheme.primary),
+                imageVector = Icons.Default.Warning,
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier
+                    .constrainAs(textTitle) {
+                        top.linkTo(image.bottom)
+                        start.linkTo(image.start)
+                    }
+                    .padding(top = 10.dp),
+                text = "Song Name",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                modifier = Modifier
+                    .constrainAs(textAuthor) {
+                        start.linkTo(image.start)
+                        top.linkTo(textTitle.bottom)
+                    },
+                text = "Author",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Spacer(modifier = Modifier.height(30.dp))
         Row(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
@@ -68,28 +88,40 @@ fun PlayerScreen(
         ) {
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
-                    imageVector = Icons.Default.ArrowForward,
-                    contentDescription = null
-                )
-            }
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
+                    modifier = Modifier.size(100.dp),
                     imageVector = Icons.Default.KeyboardArrowLeft,
                     contentDescription = null
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(
+                modifier = Modifier.padding(start = 10.dp, end = 10.dp),
+                onClick = { /*TODO*/ }
+            ) {
                 Icon(
+                    modifier = Modifier
+                        .size(100.dp),
                     imageVector = Icons.Default.PlayArrow,
                     contentDescription = null
                 )
             }
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
+                    modifier = Modifier.size(100.dp),
                     imageVector = Icons.Default.KeyboardArrowRight,
                     contentDescription = null
                 )
             }
+        }
+        Spacer(modifier = Modifier.weight(1.0f))
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = null
+            )
         }
     }
 }
