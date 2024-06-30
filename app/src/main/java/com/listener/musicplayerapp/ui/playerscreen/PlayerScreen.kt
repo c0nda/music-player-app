255370634ae903367ce278ee5d1dd6e6b64ffe83
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-06-30 15:54:39 +0400

    Added slider for tracking song progress

--- app/src/main/java/com/listener/musicplayerapp/ui/playerscreen/PlayerScreen.kt
+++ app/src/main/java/com/listener/musicplayerapp/ui/playerscreen/PlayerScreen.kt
@@ -6,9 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
@@ -20,8 +20,13 @@ import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +38,7 @@ import androidx.constraintlayout.compose.ConstraintLayout
 fun PlayerScreen(
     modifier: Modifier = Modifier
 ) {
+    var progress by rememberSaveable { mutableStateOf(0f) }
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -47,7 +53,7 @@ fun PlayerScreen(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            val (image, textTitle, textAuthor) = createRefs()
+            val (image, textTitle, textAuthor, slider, timeRow) = createRefs()
             Image(
                 modifier = Modifier
                     .constrainAs(image) {
@@ -79,8 +85,29 @@ fun PlayerScreen(
                 text = "Author",
                 style = MaterialTheme.typography.bodyMedium
             )
+            Slider(
+                value = progress,
+                modifier = Modifier
+                    .constrainAs(slider) {
+                        top.linkTo(textAuthor.bottom)
+                        start.linkTo(image.start)
+                    }
+                    .width(280.dp),
+                onValueChange = { progress = it }
+            )
+            Row(
+                modifier = Modifier
+                    .constrainAs(timeRow) {
+                        start.linkTo(image.start)
+                        top.linkTo(slider.bottom)
+                    }
+                    .width(280.dp)
+            ){
+                Text(text = "0.00")
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = "3.00")
+            }
         }
-        Spacer(modifier = Modifier.height(30.dp))
         Row(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
@@ -112,7 +139,7 @@ fun PlayerScreen(
                 )
             }
         }
-        Spacer(modifier = Modifier.weight(1.0f))
+        Spacer(modifier = Modifier.weight(1f))
         IconButton(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally),
@@ -126,7 +153,7 @@ fun PlayerScreen(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, heightDp = 640, widthDp = 360)
 @Composable
 private fun PlayerScreenPreview() {
     PlayerScreen()
