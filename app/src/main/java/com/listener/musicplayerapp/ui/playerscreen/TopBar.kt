dc6f8a031598cded950f2edf7a0b26923b92a8e8
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-01 18:12:14 +0400

    TopBar moved to separated function

---
+++ app/src/main/java/com/listener/musicplayerapp/ui/playerscreen/TopBar.kt
@@ -0,0 +1,18 @@
+package com.listener.musicplayerapp.ui.playerscreen
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowDown,
+            contentDescription = null
+        )
+    }
+}
