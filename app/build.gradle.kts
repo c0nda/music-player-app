4bd87be17b479359f319054723d08921395b7d47
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-04 21:59:09 +0400

    Changed minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.listener.musicplayerapp"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
