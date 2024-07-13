8cc4037b408a88b85dcb40800dc066384dbb1911
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-13 12:08:01 +0400

    Changed returned type of method

--- app/src/main/java/com/listener/musicplayerapp/domain/repository/SongRepository.kt
+++ app/src/main/java/com/listener/musicplayerapp/domain/repository/SongRepository.kt
@@ -1,10 +1,11 @@
 package com.listener.musicplayerapp.domain.repository
 
 import com.listener.musicplayerapp.domain.model.Song
+import com.listener.musicplayerapp.utils.Result
 import kotlinx.coroutines.flow.Flow
 
 interface SongRepository {
 
-    fun getAllSongs(): Flow<List<Song>>
+    fun getAllSongs(): Flow<Result<List<Song>>>
 
 }
