f23c202b88b2165e59a2cc9c4609e54f947f6308
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-13 12:03:38 +0400

    Added dependencies for song repository

---
+++ app/src/main/java/com/listener/musicplayerapp/di/RepositoryModule.kt
@@ -0,0 +1,14 @@
+package com.listener.musicplayerapp.di
+
+import com.listener.musicplayerapp.data.repository.SongRepositoryImpl
+import com.listener.musicplayerapp.domain.repository.SongRepository
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun provideSongRepository(songRepositoryImpl: SongRepositoryImpl): SongRepository
+
+}
