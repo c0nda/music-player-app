3d4308d8f104295cf750dbd9962d13722e9cb75f
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-14 13:18:20 +0400

    Created class with moved implementation

---
+++ app/src/main/java/com/listener/musicplayerapp/data/local/device/DeviceStorage.kt
@@ -0,0 +1,79 @@
+package com.listener.musicplayerapp.data.local.device
+
+import android.content.Context
+import android.database.Cursor
+import android.provider.MediaStore
+import com.listener.musicplayerapp.data.local.LocalDataSource
+import com.listener.musicplayerapp.domain.model.Song
+import com.listener.musicplayerapp.utils.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class DeviceStorage(private val context: Context) : LocalDataSource {
+
+    override fun getAllSongs(): Flow<Result<List<Song>>> = flow {
+
+        val songs = mutableListOf<Song>()
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.AUTHOR,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        getSongsFromExternalStorage(songs, projection)
+        getSongsFromInternalStorage(songs, projection)
+
+        if (songs.isNotEmpty()) {
+            emit(Result.Success(songs))
+        }
+    }
+
+    private fun getSongsFromExternalStorage(songs: MutableList<Song>, projection: Array<String>) {
+        val cursor = context.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            null
+        )
+        iterateAllSongs(cursor, songs)
+    }
+
+    private fun getSongsFromInternalStorage(songs: MutableList<Song>, projection: Array<String>) {
+        val cursor = context.contentResolver.query(
+            MediaStore.Audio.Media.INTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            null
+        )
+        iterateAllSongs(cursor, songs)
+    }
+
+    private fun iterateAllSongs(cursor: Cursor?, songs: MutableList<Song>) {
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    val id =
+                        cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+                    val title =
+                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME))
+                    val duration = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+                    val artist =
+                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                    songs.add(
+                        Song(
+                            id = id,
+                            songName = title,
+                            author = artist,
+                            duration = duration
+                        )
+                    )
+                } while (cursor.moveToNext())
+            }
+        }
+        cursor?.close()
+    }
+}
