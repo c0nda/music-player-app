566d453c58b814abb03c3f9ccec7c0d32004eb2f
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-17 14:58:01 +0400

    Created and implemented player controller

--- app/src/main/java/com/listener/musicplayerapp/data/service/PlayerControllerImpl.kt
+++ app/src/main/java/com/listener/musicplayerapp/data/service/PlayerControllerImpl.kt
@@ -2,15 +2,20 @@ package com.listener.musicplayerapp.data.service
 
 import android.content.ComponentName
 import android.content.Context
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
+import com.listener.musicplayerapp.domain.model.Song
 import com.listener.musicplayerapp.domain.service.PlayerController
+import com.listener.musicplayerapp.utils.PlayerState
+import com.listener.musicplayerapp.utils.toSong
 
-class PlayerControllerImpl(context: Context): PlayerController {
+class PlayerControllerImpl(context: Context) : PlayerController {
 
-    private var factory: ListenableFuture<MediaController>? = null
+    private var factory: ListenableFuture<MediaController>
     private var mediaController: MediaController? = null
 
     init {
@@ -19,9 +24,9 @@ class PlayerControllerImpl(context: Context): PlayerController {
             context,
             sessionToken
         ).buildAsync()
-        factory?.addListener(
+        factory.addListener(
             {
-                mediaController = factory?.let {
+                mediaController = factory.let {
                     if (it.isDone) {
                         it.get()
                     } else {
@@ -31,5 +36,71 @@ class PlayerControllerImpl(context: Context): PlayerController {
             },
             MoreExecutors.directExecutor()
         )
+    }
+
+    override var playerControllerCallback: (
+        (
+        playerState: PlayerState,
+        currentSong: Song?,
+        currentPosition: Long,
+        duration: Long,
+        isRepeatOneEnabled: Boolean,
+        isShuffleAllEnabled: Boolean
+    ) -> Unit)? = null
+
+    override fun addMediaItems(songs: List<Song>) {
+        val mediaItems = songs.map {
+            MediaItem.Builder()
+                .setMediaId(it.id.toString())
+                .setMediaMetadata(
+                    MediaMetadata.Builder()
+                        .setTitle(it.songName)
+                        .setArtist(it.author)
+                        .build()
+                )
+                .build()
+        }
+        mediaController?.setMediaItems(mediaItems)
+    }
+
+    override fun play(mediaItemId: Int) {
+        mediaController?.apply {
+            seekToDefaultPosition(mediaItemId)
+            playWhenReady = true
+            prepare()
+        }
+    }
+
+    override fun pause() {
+        mediaController?.pause()
+    }
+
+    override fun resume() {
+        mediaController?.play()
+    }
+
+    override fun getCurPos(): Long {
+        return mediaController?.currentPosition ?: 0L
+    }
+
+    override fun getCurSong(): Song? {
+        return mediaController?.currentMediaItem?.toSong()
+    }
+
+    override fun goToNextSong() {
+        mediaController?.seekToNext()
+    }
+
+    override fun goToPrevSong() {
+        mediaController?.seekBack()
+    }
+
+    override fun seekToPos(position: Long) {
+        mediaController?.seekTo(position)
+    }
+
+    override fun destroy() {
+        MediaController.releaseFuture(factory)
+        playerControllerCallback = null
     }
 }
--- app/src/main/java/com/listener/musicplayerapp/domain/service/PlayerController.kt
+++ app/src/main/java/com/listener/musicplayerapp/domain/service/PlayerController.kt
@@ -1,6 +1,37 @@
 package com.listener.musicplayerapp.domain.service
 
+import com.listener.musicplayerapp.domain.model.Song
+import com.listener.musicplayerapp.utils.PlayerState
+
 interface PlayerController {
 
+    var playerControllerCallback: (
+        (
+        playerState: PlayerState,
+        currentSong: Song?,
+        currentPosition: Long,
+        duration: Long,
+        isRepeatOneEnabled: Boolean,
+        isShuffleAllEnabled: Boolean
+    ) -> Unit)?
 
+    fun addMediaItems(songs: List<Song>)
+
+    fun play(mediaItemId: Int)
+
+    fun pause()
+
+    fun resume()
+
+    fun getCurPos(): Long
+
+    fun getCurSong(): Song?
+
+    fun goToNextSong()
+
+    fun goToPrevSong()
+
+    fun seekToPos(position: Long)
+
+    fun destroy()
 }
