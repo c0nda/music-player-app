b467b93d1c1cab4d482f0ff5611ffc919df8f63a
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-17 14:59:03 +0400

    Added new dependency. Clas changed to abstract

--- app/src/main/java/com/listener/musicplayerapp/di/ServiceModule.kt
+++ app/src/main/java/com/listener/musicplayerapp/di/ServiceModule.kt
@@ -6,22 +6,32 @@ import dagger.Provides
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.exoplayer.ExoPlayer
+import com.listener.musicplayerapp.data.service.PlayerControllerImpl
+import com.listener.musicplayerapp.domain.service.PlayerController
+import dagger.Binds
+import javax.inject.Singleton
 
 @Module
-class ServiceModule {
+abstract class ServiceModule {
 
-    @Provides
-    fun provideAudioAttributes() = AudioAttributes.Builder()
-        .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
-        .setUsage(C.USAGE_MEDIA)
-        .build()
+    companion object {
+        @Provides
+        fun provideAudioAttributes() = AudioAttributes.Builder()
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+            .setUsage(C.USAGE_MEDIA)
+            .build()
 
-    @Provides
-    fun provideExoPlayer(
-        context: Context,
-        audioAttributes: AudioAttributes
-    ) = ExoPlayer.Builder(context).build().apply {
-        setAudioAttributes(audioAttributes, true)
-        setHandleAudioBecomingNoisy(true)
+        @Provides
+        fun provideExoPlayer(
+            context: Context,
+            audioAttributes: AudioAttributes
+        ) = ExoPlayer.Builder(context).build().apply {
+            setAudioAttributes(audioAttributes, true)
+            setHandleAudioBecomingNoisy(true)
+        }
     }
+
+    @Singleton
+    @Binds
+    abstract fun bindPlayerController(playerControllerImpl: PlayerControllerImpl): PlayerController
 }
