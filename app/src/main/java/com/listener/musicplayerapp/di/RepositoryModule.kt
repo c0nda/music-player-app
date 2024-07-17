dce154bf5b700a72190425765559e1ace6899f3e
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-07-17 14:58:20 +0400

    Added Singleton annotation

--- app/src/main/java/com/listener/musicplayerapp/di/RepositoryModule.kt
+++ app/src/main/java/com/listener/musicplayerapp/di/RepositoryModule.kt
@@ -4,10 +4,12 @@ import com.listener.musicplayerapp.data.repository.SongRepositoryImpl
 import com.listener.musicplayerapp.domain.repository.SongRepository
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class RepositoryModule {
 
+    @Singleton
     @Binds
     abstract fun bindSongRepository(songRepositoryImpl: SongRepositoryImpl): SongRepository
 
