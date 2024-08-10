a047882091874d2af6f64c686ba2c3b19628cc37
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-10 18:57:06 +0400

    Changed type

--- app/src/main/java/com/listener/musicplayerapp/presentation/ui/mainscreen/HomeScreenUIState.kt
+++ app/src/main/java/com/listener/musicplayerapp/presentation/ui/mainscreen/HomeScreenUIState.kt
@@ -4,7 +4,7 @@ import com.listener.musicplayerapp.domain.model.Song
 
 data class HomeScreenUIState(
     val isLoading: Boolean = false,
-    val songs: List<Song?> = emptyList(),
+    val songs: List<Song>? = emptyList(),
     val currentSong: Song? = null,
     val error: String? = null
 )
