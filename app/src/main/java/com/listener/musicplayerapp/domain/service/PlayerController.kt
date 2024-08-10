cb4d3e9924deeeeaa9f0fc45201297a68f995040
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-10 18:59:57 +0400

    Changed path

--- app/src/main/java/com/listener/musicplayerapp/domain/service/PlayerController.kt
+++ app/src/main/java/com/listener/musicplayerapp/domain/service/PlayerController.kt
@@ -1,7 +1,7 @@
 package com.listener.musicplayerapp.domain.service
 
 import com.listener.musicplayerapp.domain.model.Song
-import com.listener.musicplayerapp.utils.PlayerState
+import com.listener.musicplayerapp.presentation.ui.common.PlayerState
 
 interface PlayerController {
 
