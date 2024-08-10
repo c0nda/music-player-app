15ef6d62054585edb4b669271f79c9d5f907d88e
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-10 18:59:47 +0400

    Added state machine

--- app/src/main/java/com/listener/musicplayerapp/presentation/ui/mainscreen/layout/MusicListScreen.kt
+++ app/src/main/java/com/listener/musicplayerapp/presentation/ui/mainscreen/layout/MusicListScreen.kt
@@ -1,104 +1,68 @@
 package com.listener.musicplayerapp.presentation.ui.mainscreen.layout
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.MotionLayout
-import androidx.constraintlayout.compose.MotionScene
-import com.listener.musicplayerapp.R
-import com.listener.musicplayerapp.presentation.ui.playerscreen.layout.PlayerScreen
-
-private fun songs(): List<String> {
-    return List(100) { "Song $it" }
-}
+import com.listener.musicplayerapp.presentation.ui.mainscreen.HomeScreenEvent
+import com.listener.musicplayerapp.presentation.ui.mainscreen.HomeScreenUIState
 
 
 @Composable
 fun MainListScreen(
-    modifier: Modifier = Modifier
+    onEvent: (HomeScreenEvent) -> Unit,
+    uiState: HomeScreenUIState
 ) {
     Column {
         Spacer(modifier = Modifier.height(24.dp))
-        SearchBar()
-        SongList(songs = songs())
-    }
-    CurrentMusicBottomSheet()
-}
 
-@Composable
-fun CurrentMusicBottomSheet(
-    modifier: Modifier = Modifier
-) {
-    var progress by rememberSaveable { mutableStateOf(0f) }
-    val context = LocalContext.current
-    val scene = remember {
-        context
-            .resources
-            .openRawResource(R.raw.current_music_motion_scene)
-            .readBytes()
-            .decodeToString()
-    }
-
-    MotionLayout(
-        motionScene = MotionScene(content = scene),
-        progress = progress,
-    ) {
-        Box(
-            modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.primary)
-                .layoutId("music_bottom_sheet")
-        ) {
-            Row {
-                Image(
-                    imageVector = Icons.Default.Warning,
-                    contentDescription = null
-                )
-                Column {
-                    Text(text = "Song Name")
-                    Text(text = "Author")
+        with(uiState) {
+            when {
+                isLoading -> {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .size(100.dp)
+                                .fillMaxHeight()
+                                .align(Alignment.Center)
+                                .padding(16.dp)
+                        )
+                    }
                 }
-                Spacer(modifier = Modifier.weight(1f))
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Default.PlayArrow,
-                        contentDescription = null
-                    )
+
+                !isLoading && error == null -> {
+                    if (songs != null) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.BottomCenter
+                        ) {
+                            LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+                                items(items = songs) { song ->
+                                    SongContent(
+                                        song = song,
+                                        onClickCard = {
+                                            onEvent(HomeScreenEvent.OnSongSelected(song))
+                                            onEvent(HomeScreenEvent.Play)
+                                        })
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        Box(
-            modifier = Modifier
-                .layoutId("music_player")
-        ) {
-            PlayerScreen()
-        }
     }
-}
-
-@Preview(showBackground = true, widthDp = 360, heightDp = 640)
-@Composable
-private fun MainListScreenPreview() {
-    MainListScreen()
+    CurrentMusicBottomSheet()
 }
