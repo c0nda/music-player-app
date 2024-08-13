2703a16bd1f20fea9889f7e17095c064a2569f0f
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-13 12:52:01 +0400

    Added creating of viewModel and NavHost

--- app/src/main/java/com/listener/musicplayerapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/listener/musicplayerapp/presentation/ui/MainActivity.kt
@@ -6,21 +6,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.navigation.compose.rememberNavController
+import com.listener.musicplayerapp.DI
 import com.listener.musicplayerapp.data.service.MusicService
 import com.listener.musicplayerapp.presentation.ui.common.PlayerControllerViewModel
-import com.listener.musicplayerapp.presentation.ui.common.layout.MusicPlayerApp
+import com.listener.musicplayerapp.presentation.ui.navigation.PlayerNavHost
 import com.listener.musicplayerapp.presentation.ui.theme.MusicplayerappTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val playerControllerViewModel: PlayerControllerViewModel by viewModels()
+    private val component by lazy { DI.appComponent }
+
+    private val playerControllerViewModel by viewModels<PlayerControllerViewModel> {
+        component.viewModelFactory()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MusicplayerappTheme {
-                MusicPlayerApp(
+                PlayerNavHost(
+                    navController = rememberNavController(),
                     playerControllerViewModel = playerControllerViewModel
                 )
             }
