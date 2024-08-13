5b80171349bb1a2747f2a3c74aea23b2e7ef303e
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-13 12:48:47 +0400

    Added new modules and new functions

--- app/src/main/java/com/listener/musicplayerapp/di/AppComponent.kt
+++ app/src/main/java/com/listener/musicplayerapp/di/AppComponent.kt
@@ -1,6 +1,8 @@
 package com.listener.musicplayerapp.di
 
 import android.content.Context
+import com.listener.musicplayerapp.data.service.MusicService
+import com.listener.musicplayerapp.di.viewmodel.BaseViewModelFactory
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -10,10 +12,17 @@ import javax.inject.Singleton
         ServiceModule::class,
         RepositoryModule::class,
         DataModule::class,
-        UseCaseModule::class]
+        UseCaseModule::class,
+        HomeScreenModule::class,
+        SharedModule::class
+    ]
 )
 @Singleton
 interface AppComponent {
+
+    fun viewModelFactory(): BaseViewModelFactory
+
+    fun inject(musicService: MusicService)
 
     @Component.Builder
     interface Builder {
