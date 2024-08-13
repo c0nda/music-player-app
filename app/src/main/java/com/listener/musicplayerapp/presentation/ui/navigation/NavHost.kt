1a5e955ddcfef17560ccc16dc8a1013c56499409
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-13 12:56:41 +0400

    Created NavHost

---
+++ app/src/main/java/com/listener/musicplayerapp/presentation/ui/navigation/NavHost.kt
@@ -0,0 +1,65 @@
+package com.listener.musicplayerapp.presentation.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.listener.musicplayerapp.DI
+import com.listener.musicplayerapp.presentation.ui.common.PlayerControllerViewModel
+import com.listener.musicplayerapp.presentation.ui.common.daggerViewModel
+import com.listener.musicplayerapp.presentation.ui.mainscreen.HomeScreenEvent
+import com.listener.musicplayerapp.presentation.ui.mainscreen.HomeScreenViewModel
+import com.listener.musicplayerapp.presentation.ui.mainscreen.layout.CurrentMusicBottomSheet
+import com.listener.musicplayerapp.presentation.ui.mainscreen.layout.MusicListScreen
+
+@Composable
+fun PlayerNavHost(
+    navController: NavHostController,
+    playerControllerViewModel: PlayerControllerViewModel
+) {
+    val playerControllerUIState = playerControllerViewModel.playerControllerUIState
+
+    NavHost(navController = navController, startDestination = Routes.HomeScreen.route) {
+        composable(route = Routes.HomeScreen.route) {
+            val homeScreenViewModel: HomeScreenViewModel = daggerViewModel {
+                DI.appComponent.viewModelFactory().create(HomeScreenViewModel::class.java)
+            }
+            var isInitialized by rememberSaveable { mutableStateOf(false) }
+
+            if (!isInitialized) {
+                LaunchedEffect(key1 = Unit) {
+                    homeScreenViewModel.onEvent(HomeScreenEvent.GetSong)
+                    isInitialized = true
+                }
+            }
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                MusicListScreen(
+                    onEvent = homeScreenViewModel::onEvent,
+                    uiState = homeScreenViewModel.homeScreenUIState.collectAsState().value
+                )
+                CurrentMusicBottomSheet(
+                    modifier = Modifier.align(Alignment.BottomCenter),
+                    onEvent = homeScreenViewModel::onEvent,
+                    song = playerControllerUIState.collectAsState().value.currentSong,
+                    playerState = playerControllerUIState.collectAsState().value.playerState,
+                    onBarClick = { navController.navigate(Routes.SongScreen.route) }
+                )
+            }
+        }
+
+        composable(route = Routes.SongScreen.route) {
+
+        }
+    }
+}
