8850a8287d87ccb9c3ab7fb004bf07d7d32d0ee2
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-14 15:56:44 +0400

    Removed flow builder

--- app/src/main/java/com/listener/musicplayerapp/data/repository/SongRepositoryImpl.kt
+++ app/src/main/java/com/listener/musicplayerapp/data/repository/SongRepositoryImpl.kt
@@ -5,14 +5,11 @@ import com.listener.musicplayerapp.domain.model.Song
 import com.listener.musicplayerapp.domain.repository.SongRepository
 import com.listener.musicplayerapp.utils.Result
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SongRepositoryImpl @Inject constructor(
     private val localDataSource: LocalDataSource
 ) : SongRepository {
 
-    override fun getAllSongs(): Flow<Result<List<Song>>> = flow {
-        localDataSource.getAllSongs()
-    }
+    override fun getAllSongs(): Flow<Result<List<Song>>> = localDataSource.getAllSongs()
 }
