260a7d25ed2be7e3263bc27c3fc77beaa9d3e211
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-08-24 14:31:27 +0400

    Added uri field

--- app/src/main/java/com/listener/musicplayerapp/domain/model/Song.kt
+++ app/src/main/java/com/listener/musicplayerapp/domain/model/Song.kt
@@ -1,9 +1,12 @@
 package com.listener.musicplayerapp.domain.model
 
+import android.net.Uri
+
 
 data class Song(
     val id: Int,
     val songName: String,
     val author: String,
-    val duration: Long
+    val duration: Long,
+    val uri: Uri
 )
