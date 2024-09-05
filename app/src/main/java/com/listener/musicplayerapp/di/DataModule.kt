382fdd6091bf23fb3fdf2ab27ebba92505251d41
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-09-05 12:18:24 +0400

    Added @Singleton

--- app/src/main/java/com/listener/musicplayerapp/di/DataModule.kt
+++ app/src/main/java/com/listener/musicplayerapp/di/DataModule.kt
@@ -4,10 +4,12 @@ import com.listener.musicplayerapp.data.local.LocalDataSource
 import com.listener.musicplayerapp.data.local.device.DeviceStorage
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class DataModule {
 
+    @Singleton
     @Binds
     abstract fun bindLocalDataSource(deviceStorage: DeviceStorage): LocalDataSource
 }
