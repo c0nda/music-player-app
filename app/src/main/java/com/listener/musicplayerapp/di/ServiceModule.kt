b05a564702712a381c69d734a77c49738957232d
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-09-05 12:23:49 +0400

    Added new provides methods

--- app/src/main/java/com/listener/musicplayerapp/di/ServiceModule.kt
+++ app/src/main/java/com/listener/musicplayerapp/di/ServiceModule.kt
@@ -1,11 +1,16 @@
 package com.listener.musicplayerapp.di
 
+import android.content.ComponentName
 import android.content.Context
 import dagger.Module
 import dagger.Provides
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.listener.musicplayerapp.data.service.MusicService
 import com.listener.musicplayerapp.data.service.PlayerControllerImpl
 import com.listener.musicplayerapp.domain.service.PlayerController
 import dagger.Binds
@@ -13,14 +18,16 @@ import javax.inject.Singleton
 
 @Module
 abstract class ServiceModule {
-
     companion object {
+
+        @Singleton
         @Provides
         fun provideAudioAttributes() = AudioAttributes.Builder()
             .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
             .setUsage(C.USAGE_MEDIA)
             .build()
 
+        @Singleton
         @Provides
         fun provideExoPlayer(
             context: Context,
@@ -28,6 +35,20 @@ abstract class ServiceModule {
         ) = ExoPlayer.Builder(context).build().apply {
             setAudioAttributes(audioAttributes, true)
             setHandleAudioBecomingNoisy(true)
+        }
+
+        @Singleton
+        @Provides
+        fun providePlayerControllerFuture(context: Context): ListenableFuture<MediaController> {
+            val sessionToken =
+                SessionToken(context, ComponentName(context, MusicService::class.java))
+            return MediaController.Builder(context, sessionToken).buildAsync()
+        }
+
+        @Singleton
+        @Provides
+        fun providePlayerController(factory: ListenableFuture<MediaController>): PlayerControllerImpl {
+            return PlayerControllerImpl(factory)
         }
     }
 
