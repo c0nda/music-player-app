b1b2ba49c5e176ddf35a2c0d7b4f544822367794
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-09-05 14:31:36 +0400

    Creating of mediaSession moved to di

--- app/src/main/java/com/listener/musicplayerapp/data/service/MusicService.kt
+++ app/src/main/java/com/listener/musicplayerapp/data/service/MusicService.kt
@@ -1,18 +1,16 @@
 package com.listener.musicplayerapp.data.service
 
 import android.content.Intent
-import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
-import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
 import com.listener.musicplayerapp.DI
 import javax.inject.Inject
 
 class MusicService : MediaSessionService() {
 
-    private var mediaSession: MediaSession? = null
+    @Inject
+    lateinit var mediaSession: MediaSession
 
     @Inject
     lateinit var exoPlayer: ExoPlayer
@@ -21,44 +19,25 @@ class MusicService : MediaSessionService() {
         super.onCreate()
 
         DI.appComponent.inject(this)
-
-        mediaSession = MediaSession.Builder(this, exoPlayer)
-            .setCallback(MediaSessionCallback())
-            .build()
     }
 
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession {
         return mediaSession
     }
 
     override fun onTaskRemoved(rootIntent: Intent?) {
-        val player = mediaSession?.player
+        val player = mediaSession.player
 
-        if (player == null || !player.playWhenReady || player.mediaItemCount == 0) {
+        if (!player.playWhenReady || player.mediaItemCount == 0) {
             stopSelf()
         }
     }
 
     override fun onDestroy() {
-        mediaSession?.run {
+        mediaSession.run {
             exoPlayer.release()
             release()
-            mediaSession = null
         }
         super.onDestroy()
-    }
-
-    private inner class MediaSessionCallback : MediaSession.Callback {
-
-        override fun onAddMediaItems(
-            mediaSession: MediaSession,
-            controller: MediaSession.ControllerInfo,
-            mediaItems: MutableList<MediaItem>
-        ): ListenableFuture<MutableList<MediaItem>> {
-            val updatedMediaItems = mediaItems.map {
-                it.buildUpon().setUri(it.localConfiguration?.uri).build()
-            }.toMutableList()
-            return Futures.immediateFuture(updatedMediaItems)
-        }
     }
 }
