600e6ab833149085e4d2a95a16b5d897419d9087
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-09-09 14:30:54 +0400

    Removed onTaskRemoved()

--- app/src/main/java/com/listener/musicplayerapp/data/service/MusicService.kt
+++ app/src/main/java/com/listener/musicplayerapp/data/service/MusicService.kt
@@ -1,6 +1,5 @@
 package com.listener.musicplayerapp.data.service
 
-import android.content.Intent
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
@@ -23,14 +22,6 @@ class MusicService : MediaSessionService() {
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession {
         return mediaSession
-    }
-
-    override fun onTaskRemoved(rootIntent: Intent?) {
-        val player = mediaSession.player
-
-        if (!player.playWhenReady || player.mediaItemCount == 0) {
-            stopSelf()
-        }
     }
 
     override fun onDestroy() {
